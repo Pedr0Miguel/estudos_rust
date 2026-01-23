886c1e2958277fc80e3df3fe4bb4cc3164fd43df
Pedr0Miguel <pedro.orosco2.0@gmail.com>
2026-01-23 22:16:20 +0000

    41- Review dos últimos métodos

--- learn-to-code-with-rust/variables-and-mutability/src/main.rs
+++ learn-to-code-with-rust/variables-and-mutability/src/main.rs
@@ -1,7 +1,30 @@
-#![allow(unused_variables)]
-type Metros = i32;
+const TOUCHDOWN_POINTS: i32 = 6;
+
 fn main() {
+    let season: &str = "Invincible";
+
+    let _favorite_beverage: &str = "Whiskey";
+
     #[allow(unused_variables)]
-    let altura: Metros = 1600;
-    let largura: Metros = 100;
+    let favorite_beverage2: &str = "Whiskey";
+
+    #[allow(unused_variables)]
+    let mut points_scored: i32 = 28;
+
+    points_scored = 35;
+
+    let event_time: &str = "06:00";
+
+    println!(
+        "Season: {season}; points: {points_scored}; event_time: {event_time}; event_time: {TOUCHDOWN_POINTS};"
+    );
+    println!(
+        "Season: {}; points: {}; event_time: {}; TOUCHDOWN_POINTS: {};",
+        season, points_scored, event_time, TOUCHDOWN_POINTS
+    );
+
+    println!(
+        "Season: {0}; points: {1}; event_time: {2};TOUCHDOWN_POINTS: {3};",
+        season, points_scored, event_time, TOUCHDOWN_POINTS
+    );
 }
