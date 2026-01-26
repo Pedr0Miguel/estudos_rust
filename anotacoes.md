9f2365406466b164ad2621663470852363f4b684
Pedr0Miguel <pedro.orosco2.0@gmail.com>
2026-01-26 18:03:24 +0000

    52 - Operações matematicas

--- anotacoes.md
+++ anotacoes.md
@@ -13,6 +13,31 @@ Usando o template abaixo:
 
 ---
 
+## Operações matematicas
+
+Operações matemáticas assim como em toda linguagem é os principais:
+
+```rust
+let adicao: i32 = 5+9;
+let subtracao: i32 = 5-9;
+let multiplicacao: i32 = 5*9;
+
+let floor_divisao = 5/3;
+println!("{floor_divisao}"); // isso vai printar 1 pois podemos dividir 5 por 3 apenas uma vez
+
+let floor_float = 5.00/3.00;
+println!("{floor_float}"); // vai retornar o calculo completo 1.6666
+
+let resto = 8%2;
+let resto2 = 9%2;
+
+println!("{resto}"); // vai retornar o resto da divisão de 8 por 2 que seria 0
+println!("{resto2}"); // vai retornar o resto da divisão de 9 por 2 que seria 1
+
+```
+
+---
+
 ## Título do update
 
 Descrição do que é existe. Neste momento vai ser feito assim...
--- learn-to-code-with-rust/data-types/src/main.rs
+++ learn-to-code-with-rust/data-types/src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    let miles_away = 50;
-    let miles_away_i8 = miles_away as i8;
-    let miles_away_u8 = miles_away as u8;
+    let adicao: i32 = 5 + 9;
+    let subtracao: i32 = 5 - 9;
+    let multiplicacao: i32 = 5 * 9;
+    println!("{adicao}");
+    println!("{subtracao}");
+    println!("{multiplicacao}");
 
-    let miles_float: f64 = 100.34234;
-    let miles_f32 = miles_away as f32;
-    let miles_int = miles_away as i32;
+    let floor_divisao = 5 / 3;
+    println!("{floor_divisao}"); // isso vai printar 1 pois podemos dividir 5 por 3 apenas uma vez
 
-    println!("{miles_int}");
+    let floor_float = 5.00 / 3.00;
+    println!("{floor_float}"); // vai retornar o calculo completo 1.6666
+
+    let resto = 8 % 2;
+    let resto2 = 9 % 2;
+
+    println!("{resto}"); // vai retornar o resto da divisão de 8 por 2 que seria 0
+    println!("{resto2}"); // vai retornar o resto da divisão de 9 por 2 que seria 1
 }
