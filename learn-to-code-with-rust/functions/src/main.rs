75d3e3d0d837950a1565bd3a673cfec18d194385
Pedr0Miguel <pedro.orosco2.0@gmail.com>
2026-02-02 13:55:51 +0000

    78 - Functions Challenge

--- learn-to-code-with-rust/functions/src/main.rs
+++ learn-to-code-with-rust/functions/src/main.rs
@@ -1,10 +1,25 @@
+fn apply_to_jobs(number: i32, title: &str) {
+    println!("I'm applying to {number} {title} jobs");
+}
+
+fn is_even(number: i32) -> bool {
+    if number % 2 == 0 {
+        return true;
+    }
+    return false;
+}
+
+fn alphabets(string: &str) -> (bool, bool) {
+    return (string.contains('a'), string.contains('z'));
+}
+
 fn main() {
-    let multiplicador = 3;
+    apply_to_jobs(2, "Rust developer");
 
-    let calculadora = {
-        let value = 5 + 4;
+    println!("{}", is_even(9));
+    println!("{}", is_even(12));
 
-        value * multiplicador
-    };
-    println!("{calculadora}");
+    dbg!(alphabets("aardvark"));
+    dbg!(alphabets("zoology"));
+    dbg!(alphabets("zebra"));
 }
