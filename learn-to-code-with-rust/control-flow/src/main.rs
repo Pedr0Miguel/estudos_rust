2a15666682ce0d6a14754f6738c800165173f42e
Pedr0Miguel <pedro.orosco2.0@gmail.com>
2026-02-03 00:41:53 +0000

    86 - 88 March Statement

--- learn-to-code-with-rust/control-flow/src/main.rs
+++ learn-to-code-with-rust/control-flow/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    let number: i32 = 8;
+
+    match number {
+        x if x % 2 == 0 => print!("PAR"),
+        x if x % 2 != 0 => print!("IMPAR"),
+        _ => unreachable!(),
+    };
+}
