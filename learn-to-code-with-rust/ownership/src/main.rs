ab2fb1bd4ea4f271e62a4e17fdc87081bcf59b24
Pedr0Miguel <pedro.orosco2.0@gmail.com>
2026-02-11 21:05:37 +0000

    118. Project Solution

--- learn-to-code-with-rust/ownership/src/main.rs
+++ learn-to-code-with-rust/ownership/src/main.rs
@@ -1,11 +1,27 @@
 fn main() {
+    let is_concert: bool = true;
+    let is_event: bool = is_concert;
 
-    let mut comida_atual = String::new();
-    comida_atual = add_sabor(comida_atual);
+    println!("{is_concert}, {is_event}");
+
+
+    let dinner = "Salmon";
+    let sushi = dinner;
+
+    println!("{dinner}, {sushi}");
+
+    let s_ex = String::from("Salmon");
+    let mut ex2 = s_ex;
+
+//    println!("{s_ex}, {ex2}");
+
+    ex2 = eat_meal(ex2);
+
+    println!("{ex2}");
+
 }
 
-
-fn add_sabor(mut comida: String)-> String{
-    comida.push_str(" sabor energético");
-    comida
+fn eat_meal(mut meal: String)-> String{
+    meal.clear();
+    meal
 }
