1fca505a25728e0806a16393599d9475c7a47a25
Pedr0Miguel <pedro.orosco2.0@gmail.com>
2026-02-13 23:34:26 +0000

    127. Resolvendo o coding_challenge

--- anotacoes.md
+++ anotacoes.md
@@ -19,6 +19,52 @@ Descricao
 
 ---
 
+## Resolvendo o coding_challenge
+
+Código:
+
+```rust
+fn main() {
+
+    let mut trip = start_trip();
+
+    visit_philadelphia(&mut trip);
+
+    trip.push_str(" and ");
+
+    visit_new_york(&mut trip);
+
+    trip.push_str(" and ");
+
+    visit_boston(&mut trip);
+
+    show_itinerary(&trip);
+
+}
+
+fn start_trip()-> String{
+    String::from("The plan is...")
+}
+
+fn visit_philadelphia(plan: &mut String){
+    plan.push_str("Philadephia")
+}
+
+fn visit_new_york(plan: &mut String){
+    plan.push_str("New York")
+}
+
+fn visit_boston(plan: &mut String){
+    plan.push_str("Boston")
+}
+
+fn show_itinerary(plan: &String){
+    println!("{plan}");
+}
+```
+
+---
+
 ## 126. Ownership with Arrays and Tuples
 
 Vamos ver como funciona o conceito de Ownership em um Array e Tupla.
--- learn-to-code-with-rust/ownership/src/main.rs
+++ learn-to-code-with-rust/ownership/src/main.rs
@@ -1,11 +1,37 @@
 fn main() {
-    let registros = [true, false, true];
 
-    let first = registros[0];
+    let mut trip = start_trip();
 
-    println!("{first} e array {registros:?}");
-    
-    let langs = [String::from("Rust"), String::from("Go")];
-    let first_lang = &langs[0];
-    println!("{first_lang} e array {langs:?}");
+    visit_philadelphia(&mut trip);
+
+    trip.push_str(" and ");
+
+    visit_new_york(&mut trip);
+
+    trip.push_str(" and ");
+
+    visit_boston(&mut trip);
+
+    show_itinerary(&trip);
+
+}
+
+fn start_trip()-> String{
+    String::from("The plan is...")
+}
+
+fn visit_philadelphia(plan: &mut String){
+    plan.push_str("Philadephia")
+}
+
+fn visit_new_york(plan: &mut String){
+    plan.push_str("New York")
+}
+
+fn visit_boston(plan: &mut String){
+    plan.push_str("Boston")
+}
+
+fn show_itinerary(plan: &String){
+    println!("{plan}");
 }
